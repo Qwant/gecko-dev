aee0f6b6689e82f4363420d47be4fbf9b6fe72ce
p.bertran <p.bertran@qwant.com>
2018-10-18 10:32:06 +0200

    First run code changed mozilla's side. So we adapt

--- mobile/android/base/java/org/mozilla/gecko/firstrun/FirstrunPanel.java
+++ mobile/android/base/java/org/mozilla/gecko/firstrun/FirstrunPanel.java
@@ -25,7 +25,7 @@ import org.mozilla.gecko.TelemetryContract;
  */
 public class FirstrunPanel extends Fragment {
 
-    protected boolean showBrowserHint = true;
+    protected boolean showBrowserHint = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
