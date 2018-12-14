068fd4cb756890c1524f956f93f3898f66580b57
p.bertran <p.bertran@qwant.com>
2018-12-14 11:43:06 +0100

    disable persistent notification completely (commented out)

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -905,7 +905,7 @@ pref("privacy.trackingprotection.enabled", true);
 pref("privacy.trackingprotection.value", 2);
 pref("privacy.donottrackheader.enabled", true);
 
-pref("qwant.persistentnotification.enabled", true);
+pref("qwant.persistentnotification.enabled", false);
 
 pref("qwant.distribution.loaded", false);
 
