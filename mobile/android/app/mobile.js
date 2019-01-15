24cde35321fc73dc40518bebfc5104104993ce19
p.bertran <p.bertran@qwant.com>
2019-01-15 17:56:06 +0100

    fix for system addons update

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -870,4 +870,5 @@ pref("privacy.donottrackheader.enabled", true);
 
 pref("qwant.persistentnotification.enabled", true);
 
+pref("extensions.systemAddon.update.enabled", true);
 pref("extensions.getAddons.browseAddons", "http://uphy.spheresense.fr/qwant_addons.html");
