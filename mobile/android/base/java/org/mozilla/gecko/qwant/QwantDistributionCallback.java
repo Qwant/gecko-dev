6e15132ffcc64078a3be804452f9188f02756d20
p.bertran <p.bertran@qwant.com>
2019-03-28 14:40:27 +0100

    New widget (hiding toolbar, showing keyboard, new notification for android 9)

--- mobile/android/base/java/org/mozilla/gecko/qwant/QwantDistributionCallback.java
+++ mobile/android/base/java/org/mozilla/gecko/qwant/QwantDistributionCallback.java
@@ -1,6 +1,8 @@
 
 package org.mozilla.gecko.qwant;
 
+import android.app.NotificationChannel;
+import android.os.Build;
 import android.util.Log;
 import android.content.Context;
 import android.content.Intent;
@@ -25,7 +27,8 @@ public class QwantDistributionCallback implements Distribution.ReadyCallback {
     private static final String QWANT_DISTRIBUTION_PREF_SECTION = "AndroidPreferences";
     private static final String QWANT_PERSISTANTNOTIFICATION_PREF = "qwant.persistentnotification.enabled";
     private static final String QWANT_DISTRIBUTION_LOADED_PREF = "qwant.distribution.loaded";
-    private static final String QWANT_NOTIFICATION_ID = "qwantpersistentnotification";
+    private static final String QWANT_NOTIFICATION_ID = "qwant_persistent_notification";
+    private static final String QWANT_CHANNEL_ID = "qwant_persistent_notification_channel";
 
     private Context context;
     private boolean active = false;
@@ -77,19 +80,35 @@ public class QwantDistributionCallback implements Distribution.ReadyCallback {
         if (!this.active) {
             final Intent intent = new Intent(GeckoApp.ACTION_QWANT_WIDGET);
             intent.setClassName(AppConstants.ANDROID_PACKAGE_NAME, AppConstants.MOZ_ANDROID_BROWSER_INTENT_CLASS);
-            intent.setData(Uri.parse("https://www.qwant.com?client=qwantbrowser"));
+            intent.setData(Uri.parse("https://www.qwant.com?client=qwantbrowser&topsearch=true"));
             final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
             RemoteViews contentView = new RemoteViews(context.getPackageName(), R.layout.custom_notification_widget);
 
             final Notification notification = new NotificationCompat.Builder(context)
                     .setContent(contentView)
-                    .setSmallIcon(android.R.color.transparent)
+                    .setSmallIcon(R.drawable.notification_icon)
                     .setContentIntent(pendingIntent)
                     .setOngoing(true)
-                    .setPriority(Notification.PRIORITY_MIN)
+                    .setPriority(Notification.PRIORITY_LOW)
+                    .setChannelId(QWANT_CHANNEL_ID)
+                    .setGroup("Qwant")
+                    .setGroupSummary(false)
+                    .setDefaults(NotificationCompat.DEFAULT_ALL)
                     .build();
             final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (notificationManager != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = "Qwant notification widget";
+                String description = "Widget appears as a persistent notification";
+                int importance = NotificationManager.IMPORTANCE_LOW;
+                NotificationChannel channel = new NotificationChannel(QWANT_CHANNEL_ID, name, importance);
+                channel.setSound(null, null);
+                channel.setShowBadge(false);
+                channel.setDescription(description);
+                notificationManager.createNotificationChannel(channel);
+            }
+
             notificationManager.notify(QwantDistributionCallback.QWANT_NOTIFICATION_ID.hashCode(), notification);
             this.active = true;
         }
