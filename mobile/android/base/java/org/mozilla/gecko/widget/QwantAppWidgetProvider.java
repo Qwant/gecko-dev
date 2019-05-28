102ecee84707d5ccc7e116b5fbfb5b1dd909cf4f
p.bertran <p.bertran@qwant.com>
2019-05-28 16:17:09 +0200

    Improved loading time from widget

--- mobile/android/base/java/org/mozilla/gecko/widget/QwantAppWidgetProvider.java
+++ mobile/android/base/java/org/mozilla/gecko/widget/QwantAppWidgetProvider.java
@@ -6,19 +6,19 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.Context;
 import android.app.PendingIntent;
-import android.net.Uri;
-import android.widget.ImageView;
 import android.widget.RemoteViews;
 
 import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.GeckoApp;
 import org.mozilla.gecko.R;
+import org.mozilla.gecko.db.BrowserContract;
 
 public class QwantAppWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final Intent intent = new Intent(GeckoApp.ACTION_QWANT_WIDGET);
         intent.setClassName(AppConstants.ANDROID_PACKAGE_NAME, AppConstants.MOZ_ANDROID_BROWSER_INTENT_CLASS);
+        intent.putExtra(BrowserContract.SKIP_TAB_QUEUE_FLAG, true);
 
         final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
