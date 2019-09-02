17d9cb7bf29d8538f5c6109322a276bcede0683a
p.bertran <p.bertran@qwant.com>
2019-09-02 14:35:44 +0200

    version change to 3.4

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -228,7 +228,7 @@ nsHttpHandler::nsHttpHandler()
       mLastUniqueID(NowInSeconds()),
       mSessionStartTime(0),
       mLegacyAppName("QwantMobile"),
-      mLegacyAppVersion("3.3"),
+      mLegacyAppVersion("3.4"),
       mProduct("Gecko"),
       mCompatFirefoxEnabled(false),
       mUserAgentIsDirty(true),
